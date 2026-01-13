deb124a8d025d6d6a497cac3ac10ab4b22e9707d
Lufuno Mulaudzi <vendaboy.lm@gmail.com>
2026-01-13 23:25:20 +0200

    Delete Migration

--- api/database/migrations/009_add_avatar_url_to_users.sql
+++
@@ -1,8 +0,0 @@
--- Migration: 009_add_avatar_url_to_users
--- Description: Add avatar_url column to users table
--- Run: mysql -u user -p database_name < 009_add_avatar_url_to_users.sql
-
-SET NAMES utf8mb4;
-
-ALTER TABLE `users` 
-ADD COLUMN `avatar_url` VARCHAR(500) NULL AFTER `plan`;
